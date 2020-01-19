1c5b6373e0e99b81d888632f52c558083647b72c
plothost <admin@plothost.com>
2020-01-19 16:52:05 +0200

    Create php-list-values.php

---
+++ php-list-values.php
@@ -0,0 +1,14 @@
+<?php
+echo '<b>Common PHP values:</b>' .'</br></br>';
+
+echo 'PHP Version: ' . phpversion() . '</br>';
+echo 'max_execution_time = ' . ini_get('max_execution_time') . '</br>';
+echo 'max_input_time = ' . ini_get('max_input_time') . '</br>';
+echo 'max_input_vars = ' . ini_get('max_input_vars') . '</br>';
+echo 'memory_limit = ' . ini_get('memory_limit') . '</br>';
+echo 'post_max_size = ' . ini_get('post_max_size') . '</br>';
+echo 'upload_max_filesize = ' . ini_get('upload_max_filesize') . '</br>';
+
+
+echo "</br></br></br></br></br>//2020, PlotHost - https://www.plothost.com";
+?>
